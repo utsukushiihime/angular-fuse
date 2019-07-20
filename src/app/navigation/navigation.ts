116721dd33ee0c33f85cc9ea1c259fcfc99c64b8
sercan <sercanyemen@gmail.com>
2019-07-20 13:46:35 +0300

    Updated Angular to 8.1.2, Angular Material to 8.1.1 and updated various other packages Fixed: Fuse Navigation collapsable items don't collapse/expand correctly after certain level if used more than once within the same group Fixed: Better dark mode support for Calendar App

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -869,7 +869,7 @@ export const navigation: FuseNavigation[] = [
                 icon : 'update',
                 url  : '/documentation/changelog',
                 badge: {
-                    title: '8.1.0',
+                    title: '8.1.2',
                     bg   : '#EC0C8E',
                     fg   : '#FFFFFF'
                 }
